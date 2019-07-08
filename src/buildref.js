8f031ea438da93ae36dc1334e7d9bb8049318ef1
Marijn Haverbeke <marijn@haverbeke.nl>
2019-07-08 15:10:28 +0200

    Adjust to changes in builddocs interface

--- src/buildref.js
+++ src/buildref.js
@@ -1,43 +1,31 @@
 const {gather} = require("gettypes")
 const {build} = require("builddocs")
-const {join} = require("path")
+const {join, relative} = require("path")
 
 function Mod(name) {
   this.name = name
   let dir = require.resolve(name)
   this.base = dir.replace(/[\\\/]dist[\\\/][^\\\/]*$/, "")
   this.main = join(join(this.base, "src"), name == "lezer-tree" ? "tree.ts" : "index.ts")
-}
-
-function gatherAll(obj, target) {
-  if (obj.id) target[obj.id] = obj
-  if (Object.prototype.hasOwnProperty.call(obj, "constructor")) gatherAll(obj.constructor, target)
-  if (obj.properties) for (let prop in obj.properties) gatherAll(obj.properties[prop], target)
-  if (obj.staticProperties) for (let prop in obj.staticProperties) gatherAll(obj.staticProperties[prop], target)
-  return target
+  this.relative = relative(process.cwd(), this.base)
 }
 
 exports.buildRef = function buildRef() {
   let modules = ["lezer", "lezer-tree", "lezer-generator"].map(n => new Mod(n))
 
   return modules.map(mod => {
-    let {exports, usages} = gather({filename: mod.main})
-    let imports = {"[object Object]": false, Iteration: false} // FIXME investigate
-    for (let name in usages) {
-      let sibling = modules.find(m => usages[name] && usages[name].startsWith(m.base))
-      if (sibling) imports[name] = sibling.name + "." + name
-    }
+    let items = gather({filename: mod.main})
     return {
-      module: mod.name,
+      name: mod.name,
       content: build({
         name: mod.name,
         main: join(mod.main, "../README.md"),
         allowUnresolvedTypes: false,
-        imports: [imports]
-      }, {
-        items: exports,
-        all: gatherAll({properties: exports}, Object.create(null))
-      })
+        imports: [type => {
+          let sibling = type.typeSource && modules.find(m => type.typeSource.startsWith(m.relative))
+          if (sibling) return "#" + sibling.name + "." + type.type
+        }]
+      }, items)
     }
   })
 }
