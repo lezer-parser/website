00d90ed2cd502d3642774f44eb718bd69125ca93
Marijn Haverbeke <marijn@haverbeke.nl>
2021-02-17 23:27:00 +0100

    Move to new versions of getdocs-ts and builddocs

--- src/buildref.js
+++ src/buildref.js
@@ -1,4 +1,4 @@
-const {gather} = require("gettypes")
+const {gatherMany} = require("getdocs-ts")
 const {build} = require("builddocs")
 const {join, relative} = require("path")
 
@@ -14,8 +14,8 @@ function Mod(name) {
 exports.buildRef = function buildRef() {
   let modules = ["lezer", "lezer-tree", "lezer-generator"].map(n => new Mod(n))
 
-  return modules.map(mod => {
-    let items = gather({filename: mod.main})
+  let moduleItems = gatherMany(modules.map(mod => ({filename: mod.main})))
+  return modules.map((mod, i) => {
     return {
       name: mod.shortName,
       content: build({
@@ -23,11 +23,12 @@ exports.buildRef = function buildRef() {
         anchorPrefix: mod.shortName + ".",
         main: join(mod.main, "../README.md"),
         allowUnresolvedTypes: false,
+        breakAt: 45,
         imports: [type => {
           let sibling = type.typeSource && modules.find(m => type.typeSource.startsWith(m.relative))
           if (sibling) return "#" + sibling.shortName + "." + type.type
         }]
-      }, items)
+      }, moduleItems[i])
     }
   })
 }
